c39fada08fbfd00f6cfdf45644303c8ddff6bd29
Nisheeth Bandaru <nisheeth@gatech.edu>
2016-04-06 10:23:21 -0400

    to implement messaging

---
+++ messaging-example.py
@@ -0,0 +1,13 @@
+from orderbook import OrderBook
+from ordertree import OrderTree
+from orderlist import OrderList
+from order import Order
+import sys
+
+def main():
+    #TODO: Implement a messaging based order book. Take in one order at a time 
+    # and return a message indicate the status
+    pass
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
